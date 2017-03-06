f56b76c06c4ccfa882df11a51a82312092307d33
root <michael.henkel@gmail.com>
2017-03-06 02:28:03 -0800

    fixup

--- add.sh
+++ add.sh
@@ -14,7 +14,7 @@
 ./3tccClient.py add service -n svc1 -t t2
 ./3tccClient.py create service -n svc2 -sn 2.0.0.0/24 -t t2 -cust cust2 -rt 1:2 -m l2 -cv 2
 ./3tccClient.py add service -n svc2 -t t3
-./3tccClient.py create service -n svc3 -sn 3.0.0.0/24 -t t3 -cust cust3 -rt 1:3 -m l2 -cv 0
+./3tccClient.py create service -n svc3 -sn 3.0.0.0/24 -t t3 -cust cust3 -rt 1:3 -m l2 -cv 3
 ./3tccClient.py add service -n svc3 -t t1
 
 #./3tccClient.py create service -n svc4 -sn 4.0.0.0/24 -t t2 -cust cust2 -rt 1:4
